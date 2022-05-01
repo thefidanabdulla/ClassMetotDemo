ab5ce9693cad8792d258e01f8551d1102acc0ef5
thefidanabdulla <thefidanabdulla@gmail.com>
2022-05-01 15:26:42 +0400

    LAST VERSION

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriListele(Musteri[] musteriParams)
+        {
+            foreach (var musteri in musteriParams)
+            {
+                Console.WriteLine(musteri.Id + ". " +musteri.Ad + " " + musteri.Soyad);
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,20 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "John";
+            musteri1.Soyad = "Doe";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Jane";
+            musteri2.Soyad = "Doe";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriListele(musteriler);
         }
     }
 }
